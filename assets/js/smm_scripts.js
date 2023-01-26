9d95823198aa51e157d49abb50b1a208dbe67bfd
Micah Mills <micahmills@gmail.com>
2023-01-26 16:49:44 -0500

    Moved the convo_list elements to their own folder to clean things up.

--- assets/js/smm_scripts.js
+++ assets/js/smm_scripts.js
@@ -1,9 +1,9 @@
 import "@disciple.tools/web-components";
 import "./dt-button/dt-button.js";
-import "./convo_list.js";
-import "./convo_list_item.js";
-import "./convo_modal.js";
+import "./convo_list/convo_list.js";
+import "./convo_list/convo_list_item.js";
+import "./convo_list/convo_modal.js";
+import "./convo_list/convo_list_search.js";
 import "./chat/chat_window.js";
 import "./chat/chat_message.js";
 import "./dt-icon.js";
-import "./convo_list_search.js";
